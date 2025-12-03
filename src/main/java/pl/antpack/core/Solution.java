9869d899d4cdc7e33451100834ad859db96710cd
MrFggfg <bot28@o2.pl>
2025-12-03 21:38:19 +0100

    Model and core

---
+++ src/main/java/pl/antpack/core/Solution.java
@@ -0,0 +1,22 @@
+package pl.antpack.core;
+
+import pl.antpack.model.Item;
+import java.util.List;
+
+public class Solution {
+    private final List<Item> items;
+    private final int value;
+
+    public Solution(List<Item> items, int value) {
+        this.items = items;
+        this.value = value;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
